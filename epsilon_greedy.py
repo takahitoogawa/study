af4d11c8842da40b8992c13cde88f2748d6266cb
takahitoogawa <aiueogawa217@gmail.com>
2016-10-22 14:59:51 +0900

    numpied
    I change the style of coding to numpy style.

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 
+
 class BernoulliArm() :
     def __init__(self, p):
         self.p = p
@@ -14,14 +15,14 @@ class BernoulliArm() :
             return 1.0
 
 class EpsilonGreedy() :
-    def __init__(self, epsilon, counts, values):
+    def __init__(self, epsilon, n_arms):
         self.epsilon = epsilon
-        self.counts = counts
-        self.values = values
-        # return
-    def initialize(self, n_arms):
+        self.n_arms = n_arms
         self.counts = np.zeros(n_arms)
         self.values = np.zeros(n_arms)
+    def initialize(self):
+        self.counts = np.zeros(self.n_arms)
+        self.values = np.zeros(self.n_arms)
     def select_arm(self):
         if random.random() > self.epsilon:
             # argmax needs for values to be unique?
@@ -38,45 +39,32 @@ class EpsilonGreedy() :
 # num_sim represents the number of simulations
 # horizon represents ?
 def test_algorithm(algo, arms, num_sims, horizon):
-    chosen_arms = np.zeros(num_sims * horizon)
-    rewards = np.zeros(num_sims * horizon)
-    cumulative_rewards = np.zeros(num_sims * horizon)
-    sim_nums = np.zeros(num_sims * horizon)
-    times = np.zeros(num_sims * horizon)
-    for sim in range(1, 1+num_sims):
-        algo.initialize(len(arms))
-        for t in range(1, 1+horizon):
-            index = (sim - 1) * horizon + (t - 1)
-            sim_nums[index] = sim
-            times[index] = t
-
+    chosen_arms = np.zeros((num_sims, horizon))
+    rewards = np.zeros((num_sims, horizon))
+    cumulative_rewards = np.zeros((num_sims, horizon))
+    for sim in range(num_sims):
+        algo.initialize()
+        for t in range(horizon):
             chosen_arm = algo.select_arm()
-            chosen_arms[index] = chosen_arm
+            chosen_arms[sim][t] = chosen_arm
             reward = arms[chosen_arm].draw()
-            rewards[index] = reward
-
-            if t == 1:
-                cumulative_rewards[index] = reward
+            rewards[sim][t] = reward
+            if t == 0:
+                cumulative_rewards[sim][t] = reward
             else:
-                cumulative_rewards[index] = cumulative_rewards[index-1] + reward
+                cumulative_rewards[sim][t] = cumulative_rewards[sim][t-1] + reward
             algo.update(chosen_arm, reward)
-    return [sim_nums, times, chosen_arms, rewards, cumulative_rewards]
+    return (chosen_arms, rewards, cumulative_rewards)
 
 means = np.array([0.1, 0.1, 0.1, 0.1, 0.9])
 n_arms = len(means)
 random.shuffle(means)
-
 arms = map(lambda x: BernoulliArm(x), means)
 
 for epsilon in np.arange(0.1, 0.6, 0.1):
-    algo = EpsilonGreedy(epsilon, [], [])
-    algo.initialize(n_arms)
-    results = test_algorithm(algo, arms, 5000, 250)
-
-    df = pd.DataFrame({"times": results[1], "rewards": results[3]})
-    grouped = df["rewards"].groupby(df["times"])
-
-    plt.plot(grouped.mean(), label="epsilon="+str(epsilon))
+    algo = EpsilonGreedy(epsilon, n_arms)
+    chosen_arms, rewards, cumulative_rewards = test_algorithm(algo, arms, 5000, 250)
+    plt.plot(np.mean(rewards, axis=0), label="epsilon="+str(epsilon))
 
 plt.legend(loc="best")
 plt.show()
